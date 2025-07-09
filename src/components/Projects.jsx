83fb749660f69b6a1006a2fa4d4645a84ae443a0
prasanna kumar <vprasannakumar2716@gmail.com>
2025-07-09 23:46:42 +0530

    commited

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,6 +14,24 @@ import Movie from "../assets/Movie.png";
 import BioInsta from "../assets/Bio-Insta.png";
 
 const projects = [
+  // {
+  //   title: "Tickzy AI Agent with JWT Auth",
+  //   image: Tickzy,
+  //   description:
+  //     "AI-powered ticketing system with secure JWT authentication and intelligent automation using Inngest and the Gemini API.",
+  //   techStack: [
+  //     "React.js",
+  //     "Tailwind",
+  //     "Node.js",
+  //     "Express.js",
+  //     "MongoDB",
+  //     "Inngest",
+  //     "Node Mailer",
+  //     "Gemini API",
+  //   ],
+  //   liveLink: "https://tickzy-ai-agent.netlify.app/",
+  //   githubLink: "https://github.com/prasanna-27167896/AI-Agent",
+  // },
   {
     title: "Full Stack Todo App",
     image: Todo,
